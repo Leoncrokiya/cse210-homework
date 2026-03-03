f8c6c8f1cbe881dd387b6d1e64ad270773473f67
Leon M <leoncrokiya13@gmail.com>
2026-03-03 11:50:30 +0200

    New script

---
+++ guess loop.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Whatt's the magic number? ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.Write("What's your guess?");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != num)
+        {
+            if (guess > num)
+            {
+                Console.Write("Lower: ");
+            }
+            else
+            {
+                Console.Write("Higher: ");
+            }
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        if (guess == num)
+        {
+            Console.WriteLine("Congratulations! You guessed the number.");
+        }
+    }
+}
