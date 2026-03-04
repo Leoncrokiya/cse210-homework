765ae86f3aa341b823ee3461bac9f55a69108199
Leon M <leoncrokiya13@gmail.com>
2026-03-04 15:07:57 +0200

    Functions

--- functions.cs
+++ functions.cs
@@ -4,22 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        void assignment()
-        {
-            Console.WriteLine("Welcome to the program!");
+        DisplayWelcomeMessage();
 
-            Console.Write("Please enter your name: ");
-            string name = Console.ReadLine();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
 
-            Console.Write("Please enter your favorite number: ");
-            int number = int.Parse(Console.ReadLine());
+        int squaredNumber = SquareNumber(userNumber);
 
-            int square = number * number;
-
-            Console.WriteLine($"{name}, the square of {number} is {square}");
-        }
-                
-        assignment();
+        DisplayResult(userName, squaredNumber);
     }
 
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
 }
