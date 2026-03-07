577c524bcb09cc6f7e911b445df5c6411944add6
Leon Crokiya <leoncrokiya13@gmail.com>
2026-03-07 12:42:05 +0200

    Add comparison logic to Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        int x = 5;
+        int y = 7;
+        
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        
+        else
+        {
+            Console.WriteLine("Equal");
+        }
     }
 }
