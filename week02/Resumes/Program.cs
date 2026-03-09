8521f28197ea6049d660fe4380cefcd996ade1c2
Leon M <leoncrokiya13@gmail.com>
2026-03-09 13:00:33 +0200

    50%

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    string _jobTitle = "Software Engineer";
+    string _company = "Tech Company";
+    string _firstName;
+
+    string _lastName;
+
+    int _startYear = 2020;
+    int _endYear = 2023;
+
+    public Job()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.Write("First Name: ");
+        _firstName = Console.ReadLine();
+        Console.Write("Last Name: ");
+        _lastName = Console.ReadLine();
+
+        Console.WriteLine($"Name: {_firstName} {_lastName}"); //n: l c
+        Console.WriteLine($"Jobs:"); // j:
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}"); // se (m) 19-22
     }
 }
